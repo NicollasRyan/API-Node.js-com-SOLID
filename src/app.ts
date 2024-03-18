154bb6c6e61d5f0a9e28e57a6ec84f0b43543f62
Nicollas Ryan <nbarretoduarte@gmail.com>
2024-03-18 15:11:49 -0300

    Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 export const app = fastify();
+
+export const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Nicollas",
+    email: "nicolas@gmail.com",
+  },
+});
